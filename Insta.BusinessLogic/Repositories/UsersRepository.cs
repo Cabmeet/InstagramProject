d25be0261fdae612626fb6f48e6af2684ac612b3
Lyudmila Kondrashuk <Cabmeets@gmail.com>
2019-12-24 12:00:51 +0300

    auth

---
+++ Insta.BusinessLogic/Repositories/UsersRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Insta.BusinessLogic.Repositories
+{
+    using System.Linq;
+
+    using Insta.DataAccess.Context;
+
+    class UsersRepository
+    {
+        public bool FindUser(string login, string password)
+        {
+            using (var context = new InstaContext())
+            {
+                var user = context.UserRecords.Where(n => n.Profile.UserName == "" && n.EncryptedPassword == "");
+                    .FirstOrDefalt
+            }
+
+           
+        }
+
+
+    }
+}
